54d79ef90a0be8e843478c59d2675e22a15ce7e8
dlrbdjs <gon2yq@naver.com>
2024-08-23 18:00:33 +0900

    ConstVariable 클래스 생성

---
+++ ConstVariable.java
@@ -0,0 +1,7 @@
+public class ConstVariable {
+    public static final int maxNumListLength = 3;
+//    public static final int strikeBallListLength = 2;
+    public static final int randomNumBound = 9;
+//    public static final int strikeIdx = 0;
+//    public static final int ballIdx = 1;
+}
