4e2ccec4b107bbfa28fc9a9973edd5a8e0e1c296
dlrbdjs <gon2yq@naver.com>
2024-08-31 01:40:22 +0900

    Deleted Files

--- Print.java
+++
@@ -1,9 +0,0 @@
-public class Print {
-    public void printAnswer(int[] list){
-        System.out.print("정답: ");
-        for (int i = 0; i < 3; i++){
-            System.out.printf("%d", list[i]);
-        }
-        System.out.println();
-    }
-}
