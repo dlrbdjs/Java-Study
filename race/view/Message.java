b49dd36adf78f088a09a7cf7405376b8f9c501c0
dlrbdjs <gon2yq@naver.com>
2024-09-23 22:16:42 +0900

    출력문 띄어쓰기 제거

--- race/view/Message.java
+++ race/view/Message.java
@@ -1,7 +1,7 @@
 package race.view;
 
 public enum Message {
-    START_GAME("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"),
+    START_GAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_ATTEMPTS("시도할 회수는 몇회인가요?"),
     GAME_RESULT("실행 결과"),
     RACE_RESULT_COLON(" : "),
