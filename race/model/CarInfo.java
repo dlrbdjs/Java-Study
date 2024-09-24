6af90f1264b7f78388cc9d17c89a8c66b32add68
dlrbdjs <gon2yq@naver.com>
2024-09-24 11:05:00 +0900

    접근제한자 변경, get 메소드 추가

--- race/model/CarInfo.java
+++ race/model/CarInfo.java
@@ -7,11 +7,16 @@ import static race.common.util.ConstVariable.INIT_DISTANCE;
 
 public class CarInfo {
 
-    public List<Car> carInfo = new ArrayList<>();
+    private List<Car> carInfo;
 
     public void setCarInfo(String[] carNames) {
+        carInfo = new ArrayList<>();
         for (String carName : carNames){
             carInfo.add(new Car(carName, INIT_DISTANCE));
         }
+    }
+
+    public List<Car> getCarInfo() {
+        return carInfo;
     }
 }
