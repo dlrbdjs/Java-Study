f2791cc2b4a4cbc50a4051a6c953983ca2e17b09
dlrbdjs <gon2yq@naver.com>
2024-09-27 13:20:06 +0900

    상수 변수 생성

--- race/common/util/ConstVariable.java
+++ race/common/util/ConstVariable.java
@@ -1,8 +1,13 @@
 package race.common.util;
 
 public class ConstVariable {
+    // Integer Variable
     public static final int RAND_BOUND = 10;
     public static final int MOVE_CONDITION_VALUE = 4;
     public static final int INIT_DISTANCE = 0;
+    public static final int MAX_NAME_LENGTH = 5;
+
+    // String Variable
     public static final String COMMA = ",";
+
 }
--- race/common/util/ValidationHandler.java
+++ race/common/util/ValidationHandler.java
@@ -4,10 +4,12 @@ import race.view.Message;
 
 import java.util.Arrays;
 
+import static race.common.util.ConstVariable.MAX_NAME_LENGTH;
+
 public class ValidationHandler {
     public String[] validateName(String[] names){
         Arrays.stream(names)
-                .filter(name -> name.length() > 5)
+                .filter(name -> name.length() > MAX_NAME_LENGTH)
                 .findAny()
                 .ifPresent(name -> {
                     throw new IllegalArgumentException(name + Message.INPUT_ERROR);
