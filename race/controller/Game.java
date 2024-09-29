eeb5534b66b4243d901e8ad3717f2bb4cca033bb
dlrbdjs <gon2yq@naver.com>
2024-09-30 05:36:16 +0900

    carInfo 생성자를 위한 Game 클래스 수정

--- race/controller/Game.java
+++ race/controller/Game.java
@@ -1,18 +1,31 @@
 package race.controller;
 
-import race.common.util.ValidationHandler;
+import race.model.Car;
 import race.model.CarInfo;
 import race.view.UserInput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static race.common.util.ConstVariable.INIT_DISTANCE;
 
 public class Game {
 
     UserInput userInput = new UserInput();
     Result result = new Result();
-    CarInfo info = new CarInfo();
-    ValidationHandler validation = new ValidationHandler();
+    CarInfo carInfo = new CarInfo(setCarInfo(userInput.inputName()));
 
     public void playGame(){
-        info.setCarInfo(validation.validateName(userInput.inputName()));
-        result.raceResult(info.getCarInfo(), userInput.inputAttempts());
+        result.raceResult(carInfo.getCarInfo(), userInput.inputAttempts());
     }
+
+    //CarInfo 생성을 위한..
+    public List<Car> setCarInfo(String[] carNames) {
+        List<Car> carInfo = new ArrayList<>();
+        for (String carName : carNames){
+            carInfo.add(new Car(carName, INIT_DISTANCE));
+        }
+        return carInfo;
+    }
+
 }
