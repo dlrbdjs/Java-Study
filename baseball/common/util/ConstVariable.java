568ce1439c2a3a8ca711dd332960b78bb44841be
dlrbdjs <gon2yq@naver.com>
2024-11-14 01:33:10 +0900

    Create final variables

--- baseball/common/util/ConstVariable.java
+++ baseball/common/util/ConstVariable.java
@@ -7,5 +7,14 @@ public class ConstVariable {
     public static final int MAX_NUM_LIST_LENGTH = 3;
     public static final int RANDOM_NUMBER_BOUND = 9;
 
+    public static final int USER_INPUT_INTEGER_ONE = 1;
+    public static final int USER_INPUT_INTEGER_TWO = 2;
+
+    public static final boolean GAME_CONTINUE = true;
+    public static final boolean GAME_STOP = false;
+
+    public static final boolean GENERATE_NUMBERS_CONTINUE = true;
+    public static final boolean GENERATE_NUMBERS_STOP = false;
+
     public static final List<Integer> SINGLE_DIGITS = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
 }
