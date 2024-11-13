e7ca4ae24418d6fa38f98bbe4c2b2f57b6c6cf5d
dlrbdjs <gon2yq@naver.com>
2024-11-14 02:04:57 +0900

    Add static boolean method

--- baseball/common/util/Condition.java
+++ baseball/common/util/Condition.java
@@ -4,4 +4,12 @@ public class Condition {
     public static boolean isSameNumber(int num1, int num2){
         return num1 == num2;
     }
+
+    public static boolean isZero(int num){
+        return num == 0;
+    }
+
+    public static boolean isThree(int num1){
+        return num1 == 3;
+    }
 }
