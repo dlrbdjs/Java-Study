63d182e075341de3a278be1046eb0341c12f4375
dlrbdjs <gon2yq@naver.com>
2024-11-14 16:07:17 +0900

    Create ErrorMessage Enum

---
+++ baseball/view/ErrorMessage.java
@@ -0,0 +1,18 @@
+package baseball.view;
+
+public enum  ErrorMessage {
+    INPUT_ERROR_LENGTH("정수를 3개 입력하세요"),
+    INPUT_ERROR_TYPE("정수만 입력하세요"),
+    INPUT_ERROR_SAME("서로 다른 정수를 입력하세요");
+
+
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public void print() {
+        System.out.print(this.errorMessage);
+    }
+}
