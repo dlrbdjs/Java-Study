d3c0a05e33293cccb05401c64b2d317ac48547d6
dlrbdjs <gon2yq@naver.com>
2024-11-20 19:17:15 +0900

    Modify Numbers Class using first collection

--- baseball/model/Numbers.java
+++ baseball/model/Numbers.java
@@ -9,49 +9,41 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Numbers {
-    private List<Integer> userNumbers;
-    private List<Integer> randomNumbers;
-    private List<Integer> singleDigits;
+    private List<Integer> numbers;
 
     public Numbers() {
-        initSingleDigits();
+        this.numbers = new ArrayList<>();
     }
 
-    public List<Integer> getRandomNumbers() {
-        return randomNumbers;
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
-    public List<Integer> getUserNumbers() {
-        return userNumbers;
-    }
-
-    public void getSingleDigit(int idx) {
-        addRandomNumbers(singleDigits.get(idx % singleDigits.size()));
-        singleDigits.remove(idx % singleDigits.size());
+    private int getSingleDigit(int idx) {
+        int digit = numbers.get(idx % numbers.size());
+        numbers.remove(idx % numbers.size());
+        return digit;
     }
 
     public void setUserNumbers(String inputNums) {
         validateUserInputLength(inputNums);
-        userNumbers = new ArrayList<>();
+        numbers.clear();
         IntStream.range(0, inputNums.length())
-                .forEach(idx -> userNumbers.add(Integer.parseInt(String.valueOf(inputNums.charAt(idx)))));
-        validateUserNumbers(userNumbers);
+                .forEach(idx -> numbers.add(Integer.parseInt(String.valueOf(inputNums.charAt(idx)))));
+        validateUserNumbers(numbers);
     }
 
     public void setRandomNumbers() {
-        randomNumbers = new ArrayList<>();
+        Numbers singleDigits = new Numbers();
+        singleDigits.initSingleDigits();
+        numbers.clear();
         for (int i = 0; i < ConstVariable.MAX_NUM_LIST_LENGTH; i++) {
-            getSingleDigit(RandomNumber.getRandomNumber());
+            numbers.add(singleDigits.getSingleDigit(RandomNumber.getRandomNumber()));
         }
-        initSingleDigits();
     }
 
     public void initSingleDigits() {
-        this.singleDigits = new ArrayList<>(ConstVariable.SINGLE_DIGITS);
-    }
-
-    public void addRandomNumbers(int randomNumber) {
-        this.randomNumbers.add(randomNumber);
+        this.numbers = new ArrayList<>(ConstVariable.SINGLE_DIGITS);
     }
 
     private void validateUserInputLength (String inputNumbers){
@@ -59,8 +51,9 @@ public class Numbers {
             throw new IndexOutOfBoundsException();
         }
     }
-    private void validateUserNumbers (List<Integer> userNumbers){
-        if(!Condition.isDistinctThreeDigits(userNumbers)){
+
+    private void validateUserNumbers (List<Integer> numbers){
+        if(!Condition.isDistinctThreeDigits(numbers)){
             throw new IllegalArgumentException();
         }
     }
